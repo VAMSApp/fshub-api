f843d816159cb5c1e05de5ab5aaa3876fce41e95
Mike DeVita <2991640+mikedevita@users.noreply.github.com>
2025-12-23 16:46:55 -0700

    correct invalid type definition for Airline_getPilotStats

--- src/api/airline/Airline_getPilotStats.ts
+++ src/api/airline/Airline_getPilotStats.ts
@@ -1,8 +1,8 @@
-import { FSHubApi, FSHubResponse, PilotStats } from "../../types";
+import { FSHubAirlinePilotStats, FSHubApi, FSHubResponse } from "../../types";
 import FSHubApiRequest from "../FSHubApiRequest";
 
-export default async function Airline_getPilotStats(id: number, pilotId: number, api: FSHubApi): Promise<FSHubResponse<PilotStats>> {
-    const response: FSHubResponse<PilotStats> = await FSHubApiRequest(`airline/${id}/pilot/${pilotId}/stats`, api);
+export default async function Airline_getPilotStats(id: number, pilotId: number, api: FSHubApi): Promise<FSHubResponse<FSHubAirlinePilotStats>> {
+    const response: FSHubResponse<FSHubAirlinePilotStats> = await FSHubApiRequest(`airline/${id}/pilot/${pilotId}/stats`, api);
     
     return response;
 }
